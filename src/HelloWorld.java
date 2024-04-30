f227b56561ff2701bcd22fec473a7eb4cdbe2a40
DhirajAutoTesting <dhirajsuryawanshi97686@gmail.com>
2024-04-30 16:05:57 +0530

    Name changes in add family

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class HelloWorld {
+public class HelloWorld { //code Add family
     public static void main (String[] args)throws InterruptedException{
         System.setProperty("webdriver.chrome.driver" , "C:/Selenuim webdriver/chromedriver-win64/chromedriver.exe");
         WebDriver driver= new ChromeDriver();
