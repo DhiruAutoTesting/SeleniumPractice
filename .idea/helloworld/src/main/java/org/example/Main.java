00d6a5ba0086ab518ae2733725019c96808491e2
DhiruAutoTesting <167844655+DhiruAutoTesting@users.noreply.github.com>
2024-05-03 06:28:34 -0700

    Error we get in this

---
+++ .idea/helloworld/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:/Users/DELL-PC/Downloads/chromedriver-win64/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        // Add a delay to see if the browser opens
+        Thread.sleep(5000);
+
+        driver.get("https://onehealthassist.com/");
+    }
+}
